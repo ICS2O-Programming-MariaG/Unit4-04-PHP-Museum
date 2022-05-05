27e7476ff2e7f5e52f1f6800998af23e23e9c213
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-05 19:38:16 +0000

    Added constant names for age thresholds

--- check.php
+++ check.php
@@ -4,12 +4,19 @@
   //declaring and defining variables
   $userAge = intval($_POST["user-age"]);
   $day = $_POST["day"];
+  //setting constants for age thresholds
+  //I learned that I'm supposed to put quotation marks around the constant name after a Super Linter error 
+  //Learned from https://www.w3schools.com/php/php_constants.asp
+  define("CHILD_FREE", 5);
+  define("ELDERLY_FREE", 95);
+  define("STUDENT_MINIMUM", 12);
+  define("STUDENT_MAXIMUM", 21);
   //using compound if statements to determine cost
-  if (($userAge < 5) || ($userAge > 95)) {
+  if (($userAge < CHILD_FREE) || ($userAge > ELDERLY_FREE)) {
     $cost = "You get in for free!";
   }
   //Added extra brackets around all conditions to fix error - the computer was not processing the || (or) symbol when it was outside of brackets
-  else if ((($userAge >= 12) && ($userAge <= 21)) || (($day == "Thursday") || ($day == "Tuesday"))) {
+  else if ((($userAge >= STUDENT_MINIMUM) && ($userAge <= STUDENT_MAXIMUM)) || (($day == "Thursday") || ($day == "Tuesday"))) {
     $cost = "You get a discount with student pricing!";
   }
   else {
